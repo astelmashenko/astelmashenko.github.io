76a4e74cca9d60bf68cd90dac7da6e2b73f6113b
Andrii Stelmashenko <andrey.stelmashenko@gmail.com>
2024-10-06 14:59:26 +0300

    github astro action, astro as static site

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://astelmashenko.github.io',
+    base: 'home',
     integrations: [
         mdx(),
         sitemap(),
